e80cff588624313b929eaf788c3222d939c5b424
vishalpraja777 <vishalprajapathi7@gmail.com>
2024-01-01 22:43:07 +0530

    Changed Services

--- src/data/ServiceData.jsx
+++ src/data/ServiceData.jsx
@@ -16,42 +16,42 @@ const serviceDetails = [
     {
         id: 3,
         heading: "Wadrobe Making",
-        description: "Our wardrobe-making carpentry service is dedicated to crafting personalized storage solutions that seamlessly blend style and functionality,",
+        description: "Our wardrobe service is dedicated to crafting personalized storage that blend style and functionality.",
         imageLink: "/vishal-interiors/assets/wardrobe.png",
         displayDescription: "I want to make a Wadrobe"
     },
     {
         id: 4,
         heading: "Bed Making",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
+        description: "Our bed-making service specializes in crafting sturdy, and stylish beds that guarantee comfort and durability.",
         imageLink: "/vishal-interiors/assets/bed1.jpg",
         displayDescription: "I want to make a Bed"
     },
     {
         id: 5,
         heading: "TV Unit",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
+        description: "Our TV unit service is dedicated to crafting sleek and functional units that complement your entertainment space.",
         imageLink: "/vishal-interiors/assets/livingroom.jpg",
         displayDescription: "I want to make a TV Unit"
     },
     {
         id: 6,
         heading: "Study Table",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
+        description: "Our study table carpentry service specializes in designing ergonomic and customized tables.",
         imageLink: "/vishal-interiors/assets/studytable2.jpg",
         displayDescription: "I want to make a Study Table"
     },
     {
         id: 7,
         heading: "Wood Ceiling",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
+        description: "Our wood ceiling carpentry service transforms spaces with elegant, customized wooden ceilings.",
         imageLink: "/vishal-interiors/assets/ceiling.jpg",
         displayDescription: "I want to make a Wood Ceiling"
     },
     {
         id: 8,
         heading: "Wall Panelling",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
+        description: "Our wall paneling carpentry service offers precise installations, enhancing the visual texture of your interiors.",
         imageLink: "/vishal-interiors/assets/wallpanelling.webp",
         displayDescription: "I want to make a Wall Panelling"
     }
