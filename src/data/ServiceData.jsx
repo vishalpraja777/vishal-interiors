c0c7c8b3b18210847abc93681ad0d8a48bad02f6
vishalpraja777 <vishalprajapathi7@gmail.com>
2024-01-01 22:22:11 +0530

    Changed Services data

--- src/data/ServiceData.jsx
+++ src/data/ServiceData.jsx
@@ -2,58 +2,58 @@ const serviceDetails = [
     {
         id: 1,
         heading: "Modular Kitchen",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
+        description: "Our expert craftsmen specialize in designing and installing modular kitchens tailored to your needs.",
         imageLink: "/vishal-interiors/assets/kitchenmodular.jpg",
-        displayDescription: "I want service 1"
+        displayDescription: "I want to make a Modular Kitchen"
     },
     {
         id: 2,
         heading: "Modular Office",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
+        description: "Our team excels in creating efficient and adaptable office spaces through custom modular carpentry.",
         imageLink: "/vishal-interiors/assets/modularoffice.png",
-        displayDescription: "I want service 2"
+        displayDescription: "I want to make a Modular Office"
     },
     {
         id: 3,
         heading: "Wadrobe Making",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
+        description: "Our wardrobe-making carpentry service is dedicated to crafting personalized storage solutions that seamlessly blend style and functionality,",
         imageLink: "/vishal-interiors/assets/wardrobe.png",
-        displayDescription: "I want service 3"
+        displayDescription: "I want to make a Wadrobe"
     },
     {
         id: 4,
         heading: "Bed Making",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
         imageLink: "/vishal-interiors/assets/bed1.jpg",
-        displayDescription: "I want service 4"
+        displayDescription: "I want to make a Bed"
     },
     {
         id: 5,
         heading: "TV Unit",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
         imageLink: "/vishal-interiors/assets/livingroom.jpg",
-        displayDescription: "I want service 5"
+        displayDescription: "I want to make a TV Unit"
     },
     {
         id: 6,
         heading: "Study Table",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
         imageLink: "/vishal-interiors/assets/studytable2.jpg",
-        displayDescription: "I want service 6"
+        displayDescription: "I want to make a Study Table"
     },
     {
         id: 7,
         heading: "Wood Ceiling",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
         imageLink: "/vishal-interiors/assets/ceiling.jpg",
-        displayDescription: "I want service 7"
+        displayDescription: "I want to make a Wood Ceiling"
     },
     {
         id: 8,
         heading: "Wall Panelling",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, magni.",
         imageLink: "/vishal-interiors/assets/wallpanelling.webp",
-        displayDescription: "I want service 8"
+        displayDescription: "I want to make a Wall Panelling"
     }
 ]
 
